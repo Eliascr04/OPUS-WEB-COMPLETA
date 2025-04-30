7b8ba44d42ca3de0171e4bd46edfff6b69cc4898
Cristian <Cristianbermudo1990@gmail.com>
2025-04-30 18:21:07 +0200

    cambiar el dbnombre de conexion.php

--- OPUS/conexion.php
+++ OPUS/conexion.php
@@ -4,7 +4,7 @@
 $servername = "localhost"; 
 $username = "root"; 
 $password = ""; 
-$dbname = "bibliotecamuzkiz"; 
+$dbname = "bibliotecamuskiz"; 
 
 // Crear conexión
 $conexion = new mysqli($servername, $username, $password, $dbname);
